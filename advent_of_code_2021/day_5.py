2aaf4efe515a51a017f2c47d983a4c7154a445d8
Lauri Elias <lauri.elias@flowit.ee>
2021-12-07 22:30:11 +0200

    Commit final day 5

--- advent_of_code_2021/day_5.py
+++ advent_of_code_2021/day_5.py
@@ -1,32 +1,16 @@
-import decimal
 import os
 from collections import defaultdict, namedtuple
-from decimal import Decimal
+from typing import List
+
+Line = namedtuple("Line", ["x1", "x2", "y1", "y2"])
 
 
-def lin_equ(l1, l2):
-    """https://stackoverflow.com/questions/21565994/
-    method-to-return-the-equation-of-a-straight-line-given-two-points"""
-    try:
-        m = Decimal((l2[1] - l1[1])) / Decimal(l2[0] - l1[0])
-    except decimal.DivisionByZero:
-        m = 0
-    c = l2[1] - (m * l2[0])
-
-    return m, c
-
-
-def solve_first(file_name: str) -> int:
+def read_file(file_name: str) -> List[Line]:
     with open(
         f"{os.path.dirname(os.path.realpath(__file__))}/{file_name}", "r"
     ) as f:
         data = [x.strip() for x in f.readlines()]
-    # max_x = 0
-    # max_y = 0
-    # lines_data = []
-    # field = []
-    points_visited = defaultdict(int)
-    Line = namedtuple("Line", ["x1", "x2", "y1", "y2"])
+    lines = []
     for p2p_line in data:
         line_parts = p2p_line.split(" -> ")
         start_coordinates = [int(x) for x in line_parts[0].split(",")]
@@ -37,83 +21,49 @@ def solve_first(file_name: str) -> int:
             start_coordinates[1],
             end_coordinates[1],
         )
-        # equation = lin_equ(start_coordinates, end_coordinates)
-        # lines_data.append({'start': start_coordinates,
-        # 'end': end_coordinates, 'equation': equation})
+        lines.append(line)
+
+    return lines
+
+
+def get_visited_points(
+    lines: List[Line], allow_diagonals=False
+) -> defaultdict[int]:
+    points_visited = defaultdict(int)
+    for line in lines:
         dx = abs(line.x2 - line.x1)
         dy = abs(line.y2 - line.y1)
-        if dx and dy:
-            # Skip diagonal movement
-            continue
-        if dx:
+        if line.y1 == line.y2:
             min_x = min(line.x2, line.x1)
             for i in range(min_x, min_x + dx + 1):
                 points_visited[(i, line.y1)] += 1
-        elif dy:
+        elif line.x1 == line.x2:
             min_y = min(line.y2, line.y1)
             for i in range(min_y, min_y + dy + 1):
                 points_visited[(line.x1, i)] += 1
+        elif allow_diagonals:
+            length = abs(line.x2 - line.x1)
+            dx = (line.x2 - line.x1) / length
+            dy = (line.y2 - line.y1) / length
+            for n in range(length + 1):
+                x = line.x1 + (dx * n)
+                y = line.y1 + (dy * n)
+                points_visited[(x, y)] += 1
 
-    return sum(1 for v in points_visited.values() if v > 1)
-    # lines_data.append({'start': start_coordinates,
-    # 'end': end_coordinates, 'dx': dx, 'dy': dy})
+    return points_visited
 
-    #     if start_coordinates[0] > max_x:
-    #         max_x = start_coordinates[0]
-    #     if end_coordinates[0] > max_x:
-    #         max_y = end_coordinates[0]
-    #     if start_coordinates[1] > max_y:
-    #         max_y = start_coordinates[1]
-    #     if end_coordinates[1] > max_y:
-    #         max_y = end_coordinates[1]
-    # for i in range(max_y + 1):
-    #     field.append([])
-    #     field[i] = [0] * (max_x + 1)
-    # for each in lines_data:
-    #     start_x = each['start'][0]
-    #     start_y = each['start'][1]
-    #     end_x = each['end'][0]
-    #     end_y = each['end'][1]
-    #     x_movement = abs(end_x - start_x)
-    #     y_movement = abs(end_y - start_y)
-    #     if x_movement and y_movement:
-    #         # Skip diagonal movement
-    #         continue
-    #     print(start_x, start_y, end_x, end_y)
-    #     print(x_movement, y_movement)
-    #     if y_movement > 0:
-    #         if start_y > end_y:
-    #             modifier = -1
-    #         else:
-    #             modifier = 1
-    #         for i in range(y_movement + 1):
-    #             field[start_y - (i * modifier)][start_x] += 1
-    #     elif x_movement > 0:
-    #         if start_x > end_x:
-    #             modifier = -1
-    #         else:
-    #             modifier = 1
-    #         for i in range(x_movement + 1):
-    #             field[start_y][start_x + (i * modifier)] += 1
-    #     print(field)
-    # at_least_two_seen = 0
-    # for row in field:
-    #     for column in row:
-    #         if column > 1:
-    #             at_least_two_seen += 1
-    #
-    # return at_least_two_seen
-    # for each in lines_data:
-    #     if each['equation'][0] != 0:
-    #         # Only consider vertical/horizontal
-    #         continue
-    #     print(each)
-    #     if each['start'][0] < each['end'][0]:
-    #         for x in range(each['start'][0], each['end'][0] + 1):
-    #             calculated_y = each['equation'][0] * x + each['equation'][1]
-    #             field[int(calculated_y)][x] += 1
-    #     else:
-    #         for x in range(each['end'][0], each['start'][0] + 1):
-    #             calculated_y = each['equation'][0] * x + each['equation'][1]
-    #             field[int(calculated_y)][x] += 1
-    # print(field)
+
+def solve_first(file_name: str) -> int:
+    lines = read_file(file_name)
+
+    return sum(1 for v in get_visited_points(lines).values() if v > 1)
+
+
+def solve_second(file_name: str) -> int:
+    lines = read_file(file_name)
+
+    return sum(
+        1
+        for v in get_visited_points(lines, allow_diagonals=True).values()
+        if v > 1
+    )
--- tests/test_day_5.py
+++ tests/test_day_5.py
@@ -1,4 +1,4 @@
-from advent_of_code_2021.day_5 import solve_first
+from advent_of_code_2021.day_5 import solve_first, solve_second
 
 
 def test_solve_first():
@@ -11,3 +11,15 @@ def test_solve_first():
 
     print(answer)
     assert answer == 6666
+
+
+def test_solve_second():
+    answer = solve_second("day_5_sample.txt")
+
+    print(answer)
+    assert answer == 12
+
+    answer = solve_second("day_5_my_input.txt")
+
+    print(answer)
+    assert answer == 19081
