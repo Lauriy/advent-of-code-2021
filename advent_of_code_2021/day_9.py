27f05b8c465a642226cba1a9698ccdbdefcb4f8a
Lauri Elias <lauri.elias@flowit.ee>
2021-12-09 16:36:26 +0200

    Commit day 9 part 2 initial try

--- advent_of_code_2021/day_9.py
+++ advent_of_code_2021/day_9.py
@@ -1,4 +1,24 @@
+import math
 import os
+from typing import Dict, Tuple
+
+
+def get_basin(x: int, y: int, height_map: Dict[Tuple[int, int], int]):
+    basin = [(x, y)]
+    adjacent_points = [
+        p
+        for p in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        if p in height_map
+    ]
+
+    if basin[0] == 9:
+        return []
+
+    for (x2, y2) in adjacent_points:
+        if height_map[(x2, y2)] > height_map[(x, y)]:
+            basin.extend(get_basin(x2, y2, height_map))
+
+    return basin
 
 
 def solve_first(file_name: str) -> int:
@@ -28,3 +48,35 @@ def solve_first(file_name: str) -> int:
             risk_sum += val + 1
 
     return risk_sum
+
+
+def solve_second(file_name: str) -> int:
+    with open(
+        f"{os.path.dirname(os.path.realpath(__file__))}/{file_name}", "r"
+    ) as f:
+        data = [x.strip() for x in f.readlines()]
+    height_map = {
+        (x, y): int(col)
+        for y, row in enumerate(data)
+        for x, col in enumerate(row)
+    }
+
+    basin_sizes = []
+    for (x, y), val in height_map.items():
+        adjacent_points = [
+            p
+            for p in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+            if p in height_map
+        ]
+
+        all_higher = True
+        for each in adjacent_points:
+            if height_map[each] <= val:
+                all_higher = False
+                break
+
+        if all_higher:
+            points = set(get_basin(x, y, height_map))
+            basin_sizes.append(len(points))
+
+    return math.prod(sorted(basin_sizes)[-3:])
--- tests/test_day_9.py
+++ tests/test_day_9.py
@@ -1,4 +1,4 @@
-from advent_of_code_2021.day_9 import solve_first
+from advent_of_code_2021.day_9 import solve_first, solve_second
 
 
 def test_solve_first():
@@ -11,3 +11,15 @@ def test_solve_first():
 
     print(answer)
     assert answer == 448
+
+
+def test_solve_second():
+    answer = solve_second("day_9_sample.txt")
+
+    print(answer)
+    assert answer == 1134
+
+    # answer = solve_second("day_9_my_input.txt")
+    #
+    # print(answer)
+    # assert answer == 448
