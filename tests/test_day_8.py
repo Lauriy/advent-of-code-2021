f6c11e5898b45eaf5df291c4f726a72c28b6c613
Lauri Elias <lauri.elias@flowit.ee>
2021-12-09 23:08:16 +0200

    Commit day 8 part 1, commit idea for part 2 that doesn't validate yet...

---
+++ tests/test_day_8.py
@@ -0,0 +1,25 @@
+from advent_of_code_2021.day_8 import solve_first, solve_second
+
+
+def test_solve_first():
+    answer = solve_first("day_8_sample.txt")
+
+    print(answer)
+    assert answer == 26
+
+    answer = solve_first("day_8_my_input.txt")
+
+    print(answer)
+    assert answer == 440
+
+
+def test_solve_second():
+    answer = solve_second("day_8_sample.txt")
+
+    print(answer)
+    assert answer == 61229
+
+    # answer = solve_first("day_8_my_input.txt")
+    #
+    # print(answer)
+    # assert answer == 440
